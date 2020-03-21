e02da63e57e3a77b3857bbd7fe8d1eb3d6f05f57
EmilyQiRabbit <LiuYQEmily@163.com>
2020-03-21 22:17:05 +0800

    feat: 填充内容，修改样式

--- client/pages/Template0.tsx
+++ client/pages/Template0.tsx
@@ -11,8 +11,8 @@ const basicIntro = [
     key: "basic",
     content: {
       gender: { title: "性别", customInfo: "女" },
-      age: { title: "年龄", customInfo: "27" },
-      experience: { title: "工作资历", customInfo: "1 年" }
+      age: { title: "年龄", customInfo: "28" },
+      experience: { title: "工作资历", customInfo: "2年" }
     }
   },
   {
@@ -20,7 +20,7 @@ const basicIntro = [
     key: "contact",
     content: {
       phoneNumber: { title: "电话", customInfo: "188****5610" },
-      email: { title: "Email", customInfo: "LiuYQEmily@163.com" },
+      email: { title: "Email", customInfo: "L*****y@163.com" },
       wechat: { title: "微信号", customInfo: "188****5610" }
     }
   },
@@ -47,19 +47,17 @@ const EducationTable = () => {
     <>
       <table className="edu-table">
         <tr>
-          <td contentEditable> 2011 年 9 月 ~ 2015 年 6 月</td>
-          <td contentEditable>北京邮电大学 - 通信工程</td>
-          <td contentEditable>本科</td>
+          <td contentEditable>2011 年 9 月 至 2015 年 6 月</td>
+          <td contentEditable>北京邮电大学 通信工程 本科</td>
         </tr>
         <tr>
-          <td contentEditable>2015 年 9 月 ~ 2018 年 6 月</td>
-          <td contentEditable>北京邮电大学 - 通信工程</td>
-          <td contentEditable>硕士</td>
+          <td contentEditable>2015 年 9 月 至 2018 年 6 月</td>
+          <td contentEditable>北京邮电大学 通信工程 硕士</td>
         </tr>
       </table>
-      <button className="add-btn" title="添加教育背景">
+      {/* <button className="add-btn" title="添加教育背景">
         <AddIcon />
-      </button>
+      </button> */}
     </>
   );
 };
@@ -69,22 +67,70 @@ const ExperienceList = () => {
     <>
       <section className="experience-item">
         <h3>
-          <span contentEditable>xxxx 年 x 月 ~ xxxx 年 x 月</span>
-          <span contentEditable>xxx 公司</span>
+          <span contentEditable>2018/5 至今</span>
+          <span contentEditable>石墨文档</span>
           <span contentEditable>Web 前端研发</span>
         </h3>
         <p contentEditable>
-          <span>(项目名称)：</span>
-          <span>...</span>
+          <span>石墨主站</span>
+          <span>
+            研发并维护石墨桌面、官网、广告、管理后台、内部销售工具和组件库等，对接各
+            Office SDK。
+            <br />
+            参与前端微服务和 ssr 的研发。
+          </span>
         </p>
         <p contentEditable>
           <span>技术栈：</span>
-          <span>TypeScript React Webpack ... </span>
+          <span>
+            ES6，TS，jQuery，React，Redux，npm/yarn，Webpack，NodeJS，Express，KOA，Git
+          </span>
         </p>
       </section>
-      <button className="add-btn">
+
+      <section className="experience-item">
+        <h3>
+          <span contentEditable>2017/6 至 2018/5</span>
+          <span contentEditable>贝壳金控</span>
+          <span contentEditable>Web 前端实习</span>
+        </h3>
+        <p contentEditable>
+          <span>装修分期系统 & 周报系统：</span>
+          <span>
+            “装修分期”后台管理系统 web 端开发，负责所有前端页面及交互功能。
+            <br />
+            内部孵化项目：周报系统负责人，完成周报系统需求分析、页面设计，以及前后端的研发工作。
+          </span>
+        </p>
+        <p contentEditable>
+          <span>技术栈：</span>
+          <span>
+            ES6，React（AntDesign），Redux，Webpack，NodeJS，Express，MongoDB(Mongoose)，Git
+          </span>
+        </p>
+      </section>
+
+      <section className="experience-item">
+        <h3>
+          <span contentEditable>2016/6 至 2017/1 </span>
+          <span contentEditable>北京宏华兴业科技发展有限公司</span>
+          <span contentEditable>Web 前端实习</span>
+        </h3>
+        <p contentEditable>
+          <span>收费业务系统 & 制造执行系统：</span>
+          <span>
+            项目前端主要负责人。同时负责部分后端相关工作，参与 Java
+            实体类的编写和调整，对数据的逻辑关系和表结构都有一定的了解。
+          </span>
+        </p>
+        <p contentEditable>
+          <span>技术栈：</span>
+          <span>jQuery，Bootstrap 框架，JSP，SVN</span>
+        </p>
+      </section>
+      {/* <button className="add-btn">
         <AddIcon />
-      </button>
+      </button> */}
     </>
   );
 };
