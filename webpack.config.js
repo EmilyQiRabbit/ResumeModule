25426941d5e66a2b6209b293b79d27e962f55e5e
EmilyQiRabbit <LiuYQEmily@163.com>
2020-03-22 20:45:51 +0800

    feat: splitChunks

--- webpack.config.js
+++ webpack.config.js
@@ -123,7 +123,28 @@ const loadConfig = (options = {}) => {
             ["comments"]: /^!!!/
           }
         })
-      ]
+      ],
+      splitChunks: {
+        chunks: "async",
+        minSize: 30000,
+        maxSize: 0,
+        minChunks: 1,
+        maxAsyncRequests: 6,
+        maxInitialRequests: 4,
+        automaticNameDelimiter: "~",
+        automaticNameMaxLength: 30,
+        cacheGroups: {
+          defaultVendors: {
+            test: /[\\/]node_modules[\\/]/,
+            priority: -10
+          },
+          default: {
+            minChunks: 2,
+            priority: -20,
+            reuseExistingChunk: true
+          }
+        }
+      }
     },
     plugins: [
       new webpack.NoEmitOnErrorsPlugin(),
